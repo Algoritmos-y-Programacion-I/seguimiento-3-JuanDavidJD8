3c23286f68aeefb2fbe734bc01ab9b88ebf79583
Juan David Grueso Vallecilla <1061200845@u.icesi.edu.co>
2025-10-24 12:49:07 -0500

    primer commit

--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,32 @@
 package model;
+import java.time.LocaDate;
 
 public class Incident {
-    
+    private LocalDate dateReporte;
+    private String descripcion;
+    private boolean resuelto;
+    private int horaSolucion;
+
+    public Incident(LocaDate dateReporte, String descripcion){
+        this.dateReporte = dateReporte;
+        this.descripcion = descripcion;
+        this.resuelto = false;
+        this.horaSolucion = 0,
+    }
 }
+public LocalDate getDateReport(){
+    return dateReporte;
+}
+
+public String getDescripcion(){
+    return descripcion;
+}
+
+public boolean getResuelto(){
+    return resuelto;
+}
+
+public int getHoraSolucion(){
+    return horaSolucion;
+}
+
